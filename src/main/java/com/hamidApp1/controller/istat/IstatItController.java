dcea98a395aff27bc0f4fa06abb3c1b1269d96ee
Ham Shafi <hamid.shafikani@gmail.com>
2018-10-18 16:16:06 +0200

    Add entity dependancy and report

--- src/main/java/com/hamidApp1/controller/istat/IstatItController.java
+++ src/main/java/com/hamidApp1/controller/istat/IstatItController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/istatit")
-public class IstatItController {
+public class IstatItController  {
     @Autowired
     private IstatItService istatItService;
 
